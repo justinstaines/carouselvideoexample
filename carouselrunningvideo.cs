7d4009bfe7effadc93fa0566e132113ab1f3aa74
Justin Staines <justin.staines@shumbi.com>
2021-04-16 12:16:40 +0100

    Add files via upload

---
+++ carouselrunningvideo.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Xceed.Wpf.AvalonDock.Layout;
+using System.IO;
+using System.Net.Http;
+
+namespace carouselrunningvideo
+{
+    /// <summary>
+    /// (C) Copyright 2021 Justin Staines
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+  
+
+  
+
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            
+
+            InitializeComponent();
+           
+           
+
+
+        }
+
+        private void Buttonone_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Buttontwo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Buttonthree_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
